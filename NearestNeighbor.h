8ec1634cf02d9b2e3500438d7893901ab83c732f
molasia <molasia@indiana.edu>
2017-04-04 22:48:44 -0400

    Update NearestNeighbor.h

--- NearestNeighbor.h
+++ NearestNeighbor.h
@@ -9,18 +9,18 @@ public:
   {
     for(Dataset::const_iterator c_iter=filenames.begin(); c_iter != filenames.end(); ++c_iter)
       {
-	cout << "Processing " << c_iter->first << endl;
+	//cout << "Processing " << c_iter->first << endl;
 	CImg<double> class_vectors(size*size*3, filenames.size(), 1);
 	
 	// convert each image to be a row of this "model" image
 	for(int i=0; i<c_iter->second.size(); i++)
 	  class_vectors = class_vectors.draw_image(0, i, 0, 0, extract_features(c_iter->second[i].c_str()));
-	
+
 	class_vectors.save_png(("nn_model." + c_iter->first + ".png").c_str());
       }
   }
 
-  virtual string classify(const string &filename)
+  virtual string classify(const string &filename, int actualClass = 0)
   {
     CImg<double> test_image = extract_features(filename);
 	      
@@ -31,7 +31,7 @@ public:
       for(int row=0; row<models[ class_list[c] ].height(); row++)
 	if((this_cost = (test_image - models[ class_list[c] ].get_row(row)).magnitude()) < best.second)
 	  best = make_pair(class_list[c], this_cost);
-
+    cout <<best.first<<endl;
     return best.first;
   }
 
